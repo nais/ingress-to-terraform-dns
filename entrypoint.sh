89922db5a8437e0135b7470ab96c0aaab0f72ac6
Johnny Horvi <johnny@horvi.no>
2020-08-22 10:07:58 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,14 @@ if ! node /app/index.js > records.tf; then
   exit 1
 fi
 
-git clone "https://jhrv:$GH_TOKEN@github.com/navikt/iac"
+git clone "https://$GH_TOKEN@github.com/navikt/iac"
 
-mv records.tf "iac/${RECORDS_FILE_PATH}"
+cd iac
 
-cat "iac/${RECORDS_FILE_PATH}"
+mv ../records.tf "${RECORDS_FILE_PATH}"
+
+cat "${RECORDS_FILE_PATH}"
 
 #TODO uncomment this when tested ok
-#git commit -m "added custom records" || echo "No changes to commit"
+#git commit ${RECORDS_FILE_PATH} -m "added custom records" || echo "No changes to commit"
 #git push origin master
