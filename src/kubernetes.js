2473c43bbfe68738279ff889004b75de7ccb2346
Roger Bjørnstad <roger.bjornstad@nav.no>
2022-09-06 14:18:06 +0200

    Upgraded kubernetes client to support networking v1
    Co-authored-by: Terje Sannum <terje.sannum@nav.no>
    Co-authored-by: ybelmekk <youssef.bel.mekki@nav.no>

--- src/kubernetes.js
+++ src/kubernetes.js
@@ -3,7 +3,7 @@ import k8sClient from "@kubernetes/client-node";
 export const getIngressesOrDie = async () => {
     const kc = new k8sClient.KubeConfig();
     kc.loadFromDefault();
-    const k8sApi = kc.makeApiClient(k8sClient.NetworkingV1beta1Api);
+    const k8sApi = kc.makeApiClient(k8sClient.NetworkingV1Api);
 
     try {
         const payload = await k8sApi.listIngressForAllNamespaces();
